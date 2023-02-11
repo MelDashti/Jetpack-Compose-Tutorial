34f73f70ce58a4ef7802648e4eb6333370a48b84
Milad Dashti <72992585+MelDashti@users.noreply.github.com>
2023-02-11 22:40:25 +0330

    animations

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -1,15 +1,19 @@
 import android.content.res.Configuration
 import android.os.Bundle
+import android.provider.Telephony.Sms.Conversations
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -17,16 +21,85 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposebasics.R
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsTheme() {
-                Surface() {
-                    MessageCard(Message("Android", "JetpackCompose"))
-                }
+//                Surface() {
+//                    val conversationSample = listOf(
+//                        Message(
+//                            "Colleague",
+//                            "Test...Test...Test..."
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "List of Android versions:\n" +
+//                                    "Android KitKat (API 19)\n" +
+//                                    "Android Lollipop (API 21)\n" +
+//                                    "Android Marshmallow (API 23)\n" +
+//                                    "Android Nougat (API 24)\n" +
+//                                    "Android Oreo (API 26)\n" +
+//                                    "Android Pie (API 28)\n" +
+//                                    "Android 10 (API 29)\n" +
+//                                    "Android 11 (API 30)\n" +
+//                                    "Android 12 (API 31)\n"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "I think Kotlin is my favorite programming language.\n" +
+//                                    "It's so much fun!"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Searching for alternatives to XML layouts..."
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Hey, take a look at Jetpack Compose, it's great!\n" +
+//                                    "It's the Android's modern toolkit for building native UI." +
+//                                    "It simplifies and accelerates UI development on Android." +
+//                                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "It's available from API 21+ :)"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Android Studio next version's name is Arctic Fox"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "I didn't know you can now run the emulator directly from Android Studio"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Compose Previews are great to check quickly how a composable layout looks like"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Previews are also interactive after enabling the experimental setting"
+//                        ),
+//                        Message(
+//                            "Colleague",
+//                            "Have you tried writing build.gradle with KTS?"
+//                        )
+//                    )
+//                    Conversation(messages = conversationSample)
+//                    MessageCard(Message("Android", "JetpackCompose"))
+//                }
             }
         }
     }
@@ -46,21 +119,43 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // isExpanded is used to keep track of expanded state.
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // we toggle isExpanded when user clicks on column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style =
                 MaterialTheme.typography.bodyLarge
             )
+            Spacer(modifier = Modifier.height(4.dp))
             Surface(shape = MaterialTheme.shapes.medium, tonalElevation = 1.dp) {
-                Text(text = msg.body)
+                Text(
+                    text = msg.body, modifier = Modifier.padding(all = 4.dp),
+                    //if the is message is expanded, all the content is displayed.
+                    // otherwise only the first line is displayed.
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
         }
     }
 }
 
 data class Message(val author: String, val body: String)
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
 
 //you cannot preview composable functions with parameters directly so we create a new function
 @Preview
@@ -73,7 +168,74 @@ data class Message(val author: String, val body: String)
 fun previewComposableFunc() {
     JetpackComposeBasicsTheme() {
         Surface() {
-            MessageCard(msg = Message("Psychology", "The study of human nature"))
+            val conversationSample = listOf(
+                Message(
+                    "Colleague",
+                    "Test...Test...Test..."
+                ),
+                Message(
+                    "Colleague",
+                    "List of Android versions:\n" +
+                            "Android KitKat (API 19)\n" +
+                            "Android Lollipop (API 21)\n" +
+                            "Android Marshmallow (API 23)\n" +
+                            "Android Nougat (API 24)\n" +
+                            "Android Oreo (API 26)\n" +
+                            "Android Pie (API 28)\n" +
+                            "Android 10 (API 29)\n" +
+                            "Android 11 (API 30)\n" +
+                            "Android 12 (API 31)\n"
+                ),
+                Message(
+                    "Colleague",
+                    "I think Kotlin is my favorite programming language.\n" +
+                            "It's so much fun!"
+                ),
+                Message(
+                    "Colleague",
+                    "Searching for alternatives to XML layouts..."
+                ),
+                Message(
+                    "Colleague",
+                    "Hey, take a look at Jetpack Compose, it's great!\n" +
+                            "It's the Android's modern toolkit for building native UI." +
+                            "It simplifies and accelerates UI development on Android." +
+                            "Less code, powerful tools, and intuitive Kotlin APIs :)"
+                ),
+                Message(
+                    "Colleague",
+                    "It's available from API 21+ :)"
+                ),
+                Message(
+                    "Colleague",
+                    "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+                ),
+                Message(
+                    "Colleague",
+                    "Android Studio next version's name is Arctic Fox"
+                ),
+                Message(
+                    "Colleague",
+                    "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+                ),
+                Message(
+                    "Colleague",
+                    "I didn't know you can now run the emulator directly from Android Studio"
+                ),
+                Message(
+                    "Colleague",
+                    "Compose Previews are great to check quickly how a composable layout looks like"
+                ),
+                Message(
+                    "Colleague",
+                    "Previews are also interactive after enabling the experimental setting"
+                ),
+                Message(
+                    "Colleague",
+                    "Have you tried writing build.gradle with KTS?"
+                )
+            )
+            Conversation(messages = conversationSample)
         }
     }
 }
